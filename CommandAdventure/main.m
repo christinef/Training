90df4103bb36190e09df4e727a9bf134c06d3759
Christine Franks <cfranks@cfranks-mbp.thefacebook.com>
2013-07-02 14:48:50 -0700

    Initial Commit

---
+++ CommandAdventure/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CommandAdventure
+//
+//  Created by Christine Franks on 7/2/13.
+//  Copyright (c) 2013 Christine Franks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
