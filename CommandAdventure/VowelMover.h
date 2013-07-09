0b34c587a300fe6577106260bc1e84e99f826686
christinef <cfranks@cfranks-mbp.local>
2013-07-09 14:13:24 -0700

    Over and out.

---
+++ CommandAdventure/VowelMover.h
@@ -0,0 +1,13 @@
+//
+//  VowelMover.h
+//  CommandAdventure
+//
+//  Created by Christine Franks on 7/8/13.
+//  Copyright (c) 2013 Christine Franks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VowelMover : NSObject
+
+@end
---
+++ CommandAdventure/VowelMover.m
@@ -0,0 +1,13 @@
+//
+//  VowelMover.m
+//  CommandAdventure
+//
+//  Created by Christine Franks on 7/8/13.
+//  Copyright (c) 2013 Christine Franks. All rights reserved.
+//
+
+#import "VowelMover.h"
+
+@implementation VowelMover
+
+@end
